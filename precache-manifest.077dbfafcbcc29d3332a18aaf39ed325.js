f4a7f0a0164b4160563a54e7ed103c50e9daa972
Alejandro Soto <equistres@gmail.com>
2019-02-26 00:22:33 -0300

    Updates

---
+++ precache-manifest.077dbfafcbcc29d3332a18aaf39ed325.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ddfe331b0e97f394821",
+    "url": "/pokedex/static/css/main.295eda7a.chunk.css"
+  },
+  {
+    "revision": "0ddfe331b0e97f394821",
+    "url": "/pokedex/static/js/main.0ddfe331.chunk.js"
+  },
+  {
+    "revision": "c92f8f4bce56a57764ce",
+    "url": "/pokedex/static/js/runtime~main.c92f8f4b.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/pokedex/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "79328bc7efe9f137d374f922d9ee164c",
+    "url": "/pokedex/index.html"
+  }
+];
